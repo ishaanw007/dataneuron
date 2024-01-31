ba56cf1773c69ccfcaace5298b08d33a8d65efab
Ishaan <walechai019@gmail.com>
2024-01-31 15:18:24 +0530

    ishaan

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+// App.js
+
+import React from 'react';
+import { ResizableBox } from 'react-resizable';
+import 'react-resizable/css/styles.css';
 import './App.css';
 
-function App() {
+const App = () => {
+  const handleResize = (index, { size }) => {
+    // Handle resizing logic here
+    console.log(`Resized component ${index} to ${size.width}x${size.height}`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <ResizableBox className="resizable-component" width={200} height={300} onResize={(e, data) => handleResize(1, data)}>
+        <div>Component 1</div>
+      </ResizableBox>
+
+      <ResizableBox className="resizable-component" width={200} height={300} onResize={(e, data) => handleResize(2, data)}>
+        <div>Component 2</div>
+      </ResizableBox>
+
+      <ResizableBox className="resizable-component" width={200} height={300} onResize={(e, data) => handleResize(3, data)}>
+        <div>Component 3</div>
+      </ResizableBox>
     </div>
   );
-}
+};
 
 export default App;
